95f32fde4e50093ae06fe062fb9ac74726ea1d80
iuriseara <iuriseara@gmail.com>
2021-11-03 16:58:04 -0400

    Logo changes

--- components/Nav.js
+++ components/Nav.js
@@ -30,6 +30,21 @@ const Nav = () => {
       dark: '#171717'
   }
 
+  const Logo = () => {
+    return (
+        <svg 
+        width={38}
+        height={38} 
+        fill={color[colorMode]}
+        id="Layer_1" data-name="Layer 1" 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 150.77 269.8">
+        <path d="M468.15,398.6a33.5,33.5,0,1,1-33.5-33.5A33.13,33.13,0,0,1,468.15,398.6Zm-63.39,56.91h35.3c19.45,0,23.77,5.41,24.49,17.65,0,88.26.36,73.49.36,161.74H431.05c-21.25,0-26.29-7.2-26.29-26.3Z" transform="translate(-401.15 -365.1)"/>
+        <path d="M484.92,601.4a33.5,33.5,0,1,1,33.5,33.5A33.13,33.13,0,0,1,484.92,601.4Zm63.4-56.91H513c-19.46,0-23.78-5.41-24.5-17.65,0-88.26-.36-73.49-.36-161.74H522c21.25,0,26.3,7.2,26.3,26.3Z" transform="translate(-401.15 -365.1)"/>
+        </svg>
+    );
+  }
+
   return (
     <Flex height="15vh">
       <Flex
@@ -41,7 +56,7 @@ const Nav = () => {
         w="100vw"
       >
       <Box p="2">
-          <Heading color={color[colorMode]} as='h1' size="xl" fontWeight="bold">Iuri Seara</Heading>
+            <Logo/>
       </Box>
       <Spacer/>
         {/* Desktop */}
